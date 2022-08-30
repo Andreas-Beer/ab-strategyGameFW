8a45fc14259ecdc3a67e6a49eeaf9850b7c3c76b
Andreas Beer <andreasbeer@gmx.com>
2022-08-30 20:35:09 +0200

    add item tests

--- src/modules/items.test.js
+++ src/modules/items.test.js
@@ -1,18 +1,38 @@
-import { getItemDefinition } from "./items.js";
+import { getItemDefinition, checkLiquidity } from "./items.js";
 
-describe("sum module 1", () => {
-  describe("sum module 2", () => {
-    test("adds 1 + 2 to equal 3", () => {
-      expect(1 + 2).toBe(3);
+const fixtureItem1 = {
+  id: 1,
+  price: [
+    { resourceId: 1, amount: 100 },
+    { resourceId: 2, amount: 100 },
+  ],
+};
+const fixtureData = {
+  resources: { 1: 100, 2: 100 },
+};
+
+describe("items.js", () => {
+  describe("getItemDefinition()", () => {
+    test("should get right item definition", () => {
+      const getItemDefinitionResult = getItemDefinition(
+        [fixtureItem1],
+        fixtureItem1.id
+      );
+      expect(getItemDefinitionResult).toBe(fixtureItem1);
+      expect(getItemDefinitionResult.error).not.toBeDefined;
     });
-    test("adds 1 + 2 to equal 3", () => {
-      expect(1 + 2).not.toBe(4);
+    test("should return error if item definition was not found", () => {
+      const getItemDefinitionResult = getItemDefinition([fixtureItem1], 123823);
+      expect(getItemDefinitionResult.error).toBeDefined;
     });
-    test("adds 1 + 2 to equal 3", () => {
-      expect(getItemDefinition).toBeDefined();
-    });
-    test("adds 1 + 2 to equal 3", () => {
-      expect(typeof getItemDefinition).toEqual("function");
+  });
+  describe("checkLiquidity()", () => {
+    test("should return true if is liquid", () => {
+      const liquidity = checkLiquidity(
+        fixtureData.resources,
+        fixtureItem1.price
+      );
+      expect(liquidity).toBe(true);
     });
   });
 });
