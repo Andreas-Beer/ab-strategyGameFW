9b15275bb8fabe8e55aa4d4fc86618c6b0b767db
Andreas Beer <andreasbeer@gmx.com>
2022-09-02 16:29:58 +0200

    add tests to item.ts

--- src/modules/items.spec.ts
+++ src/modules/items.spec.ts
@@ -1,10 +1,12 @@
 import { expect } from "chai";
-import { findItemConfig, checkLiquidity } from "./items";
+import { findItemConfig, errors, test } from "./items";
 
 const item1: ItemConfig = { id: 1 } as ItemConfig;
 
-const resources: ResourceData = { 1: 2 };
-const prices: Price[] = [{ resourceId: 1, amount: 100 }];
+const resources: ResourceData = { 1: 100 };
+const price1: Price = { resourceId: 1, amount: 10 };
+const price2: Price = { resourceId: 1, amount: 1000 };
+const priceX: Price = { resourceId: 9999, amount: 1000 };
 
 describe("items", () => {
   describe("findItemConfig", () => {
@@ -12,18 +14,30 @@ describe("items", () => {
       const itemDefinition = findItemConfig([item1], 1);
       expect(itemDefinition.value).to.be.eq(item1);
     });
-    it("should return an error if the given id is not in the list", () => {
+    it("should return an ItemNotFoundError if the given id is not in the list", () => {
       const itemDefinition = findItemConfig([item1], 2);
       expect(itemDefinition.value).not.to.be.undefined;
-      expect(itemDefinition.value instanceof Error).to.be.eq(true);
+      expect(itemDefinition.value instanceof errors.ItemNotFoundError).to.be.eq(
+        true
+      );
     });
   });
 
-  describe("checkLiquidity", () => {
-    it("should return an Error if the resource does not exist", () => {
-      const liquidityCheck = checkLiquidity({}, prices);
-
-      expect(liquidityCheck).not.to.be.undefined;
+  describe("checkResourceAmount", () => {
+    it("should return true if the price covers the stack", () => {
+      const liquidityCheck = test.checkResourceAmount(resources, price1);
+      expect(liquidityCheck.value).to.be.true;
+    });
+    it("should return an ResourceNotFoundError if the resource is unknown", () => {
+      const liquidityCheck = test.checkResourceAmount(resources, priceX);
+      expect(liquidityCheck.value instanceof errors.ResourceNotFoundError).to.be
+        .true;
+    });
+    it("should return an ResourceNotEnoughAmountError if the price don't covers the stack", () => {
+      const liquidityCheck = test.checkResourceAmount(resources, price2);
+      expect(
+        liquidityCheck.value instanceof errors.ResourceNotEnoughAmountError
+      ).to.be.true;
     });
   });
 });
--- src/modules/items.ts
+++ src/modules/items.ts
@@ -1,5 +1,16 @@
 class ItemNotFoundError extends Error {
   public type = "ITEM_NOT_FOUND_ERROR";
+  public category = "CRITICAL";
+}
+
+class ResourceNotFoundError extends Error {
+  public type = "RESOURCE_NOT_FOUND_ERROR";
+  public category = "CRITICAL";
+}
+
+class ResourceNotEnoughAmountError extends Error {
+  public type = "RESOURCE_NOT_ENOUGH_AMOUNT";
+  public category = "NORMAL";
 }
 
 function findItemConfig(itemConfigs: ItemConfig[], itemId: number): Result {
@@ -13,6 +24,42 @@ function findItemConfig(itemConfigs: ItemConfig[], itemId: number): Result {
   return { success: true, value: foundDefinition };
 }
 
+function checkResourceAmount(resources: ResourceData, price: Price): Result {
+  const { amount, resourceId } = price;
+  const resource = resources[resourceId];
+
+  if (!resource) {
+    return {
+      success: false,
+      value: new ResourceNotFoundError(
+        `The id ${resourceId} is not an valid resource`
+      ),
+    };
+  }
+
+  const isLiquid = resource >= amount;
+  if (!isLiquid) {
+    return {
+      success: false,
+      value: new ResourceNotEnoughAmountError(
+        `The resource ${resourceId}:${resource} is not enough for ${amount}`
+      ),
+    };
+  }
+
+  return { success: true, value: true };
+}
+
 function checkLiquidity(resources: ResourceData, prices: Price[]): Result {}
 
-export { findItemConfig, checkLiquidity };
+const test = {
+  checkResourceAmount,
+};
+
+const errors = {
+  ItemNotFoundError,
+  ResourceNotFoundError,
+  ResourceNotEnoughAmountError,
+};
+
+export { test, errors, findItemConfig, checkLiquidity };
