4c02640da3e7acf63f84fd055649020951031286
Andreas Beer <andreasbeer@gmx.com>
2022-09-10 10:04:23 +0200

    add sinon-chai

--- src/helpers/logger.spec.ts
+++ src/helpers/logger.spec.ts
@@ -1,11 +1,14 @@
-import { expect } from "chai";
+import { expect, use } from "chai";
 import * as sinon from "sinon";
+import sinonChai from "sinon-chai";
 import { logger } from "./logger";
+use(sinonChai);
 
 describe("helpers/logger.ts", () => {
   it("should be a proxy of the console", () => {
-    const spy = sinon.spy(console, "log");
+    const loggerDouble = sinon.stub(console, "log");
     logger.log("foo");
-    expect(spy.called).to.be.true;
+    expect(loggerDouble).to.have.been.called;
+    loggerDouble.restore();
   });
 });
